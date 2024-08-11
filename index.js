8ea2487f4299a042075cad5c1573d566006116e8
zaheenrakib <rakibislambest@gmail.com>
2024-08-11 11:05:12 +0600

    Add Menu Item

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function run() {
     }
 
     // use verify admin after VerifyToken
-    const verifyAdmin = async (req , res,next) =>{
+    const verifyAdmin = async (req , res , next) =>{
       const email = req.decoded.email;
       const query = { email:email }
       const user = await usersCollection.findOne(query);
@@ -147,11 +147,32 @@ async function run() {
 
 
     //menu related apis
-    // Insert a document into the collection
+    // Insert a document into the collection`
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
+
+    app.get('/menu/:id' , async (req , res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req,res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete('/menu/:id', verifyToken,verifyAdmin, async (req,res) =>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
