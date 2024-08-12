9efb4f6878ce31d41134ebea991f478da457feee
zaheenrakib <rakibislambest@gmail.com>
2024-08-12 17:34:01 +0600

    Add Payment Gateway

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -227,6 +228,23 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+
+    //payment intent
+    app.post('/create-payment-intent' , async(req,res) => {
+      const { price} = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ping: 1});
